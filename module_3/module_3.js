dc4d0f3dc31342038149446f9c07f41724ffee65
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-03-25 17:08:35 +0200

    sdac

--- module_3/module_3.js
+++ module_3/module_3.js
@@ -716,4 +716,21 @@
 
 
 
+// Функция addOverNum() считает сумму всех аргументов. Измени параметры и тело функции addOverNum() так,
+//  чтобы она считала сумму только тех аргументов, которые больше чем заданное число.
+//  Это число должно быть первым параметром функции.
 
+function addOverNum(number, ...args) {
+  let total = 0;
+
+  for (const arg of args) {
+    if (arg > number) {
+    total += arg;
+    }
+  }
+  return total;
+}
+addOverNum(50, 15, 27);
+addOverNum(10, 12, 4, 11, 48, 10, 8);
+addOverNum(15, 32, 6, 13, 19, 8);
+addOverNum(20, 74, 11, 62, 46, 12, 36);
