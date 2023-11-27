f71266c23cc110fccd38413500a71bb375a2113c
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-11-27 23:07:22 +0200

    Update example_codeWars1.js

--- example/example_codeWars1.js
+++ example/example_codeWars1.js
@@ -70,4 +70,56 @@
 
 
 
-// =========== 
+// =========== Move the first letter of each word to the end of it, 
+// then add "ay" to the end of the word. Leave punctuation marks untouched.
+// Переместите первую букву каждого слова в конец, затем добавьте «ау» в конец слова.
+//  Знаки препинания оставляйте нетронутыми.
+
+// function pigIt(str) {
+//     const arr = str.split(' ')
+//     return arr
+//         .map((word) => {
+//         return word.match(/[A-z]/i)
+//             ? `${word.substr(1)}${word.substr(0, 1)}ay`
+//             : word
+//         })
+//         .join(' ')
+//     }
+// console.log(pigIt('Pig latin is cool'));
+
+
+// ============This time we want to write calculations using
+// functions and get the results. Let's have a look at some examples:
+// На этот раз мы хотим написать вычисления с использованием функций и 
+// получить результаты. Давайте посмотрим на несколько примеров:
+
+// function zero(func)   { return func ? func(0) : 0; };
+// function one(func)    { return func ? func(1) : 1; };
+// function two(func)    { return func ? func(2) : 2; };
+// function three(func)  { return func ? func(3) : 3; };
+// function four(func)   { return func ? func(4) : 4; };
+// function five(func)   { return func ? func(5) : 5; };
+// function six(func)    { return func ? func(6) : 6; };
+// function seven(func)  { return func ? func(7) : 7; };
+// function eight(func)  { return func ? func(8) : 8; };
+// function nine(func)   { return func ? func(9) : 9; };
+
+// function plus( b ) { return function( a ) { return a + b; }; };
+// function minus( b ) { return function( a ) { return a - b; }; };
+// function times( b ) { return function( a ) { return a * b; }; };
+// function dividedBy( b ) { return function( a ) { return Math.trunc(a / b); }; };
+
+// console.log(seven(times(five()))); // must return 35
+// console.log(four(plus(nine()))); // must return 13
+// console.log(eight(minus(three()))); // must return 5
+// console.log(six(dividedBy(two()))); // must return 3
+
+
+
+// =========== Write a function, which takes a non-negative integer (seconds)
+//  as input and returns the time in a human-readable format (HH:MM:SS)
+
+
+function humanReadable (seconds) {
+    return '';
+  }
